1507e9e6446b65415ff1a96d219769379a7a3ec9
Miroslav <heeymaansnapster@gmail.com>
2024-04-01 14:25:27 +0300

    completed 3-5 module 1 tasks

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,6 @@
 function ucFirst(str) {
   // ваш код...
+  if (str === "") {
+    return str;
+  } else return str.slice(0, 1).toUpperCase() + str.slice(1);
 }
--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,6 @@
 function checkSpam(str) {
   // ваш код...
+  if (str.toLowerCase().includes("xxx") || str.toLowerCase().includes("1xbet"))
+    return true;
+  else return false;
 }
